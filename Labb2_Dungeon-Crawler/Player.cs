b703ae903f4acf89656fa1829f5ead8d08ef9874
Andreas Lind-Sahlin <andreas.lind.sahlin@iths.se>
2024-09-25 19:03:49 +0200

    Added files for required classes.

---
+++ Labb2_Dungeon-Crawler/Player.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labb2_Dungeon_Crawler
+{
+    internal class Player
+    {
+    }
+}
